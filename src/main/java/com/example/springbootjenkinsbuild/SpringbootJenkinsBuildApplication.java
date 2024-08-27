1b40c752571506ea7815feaddd98a9b7ba7d66f8
steventidd <steven.tidd@gmail.com>
2024-08-27 14:13:10 -0400

    first commit

---
+++ src/main/java/com/example/springbootjenkinsbuild/SpringbootJenkinsBuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjenkinsbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsBuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJenkinsBuildApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootjenkinsbuild/SpringbootJenkinsBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjenkinsbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsBuildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
